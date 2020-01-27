6ec453264d1a5006b97d961be69ca993bc296713
Jeremy Ivan <59781543+JeremyI@users.noreply.github.com>
2020-01-26 21:59:30 -0700

    Finished Initial Coding

---
+++ AbstractFactory/Assets/AbstractFactory.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class AbstractFactory: MonoBehaviour
+{
+	public GliderFactory gliderFactory;
+	public AircraftFactory aircraftFactory;
+	
+	public void SpawnItem(Requirements reqs)
+	{
+		Debug.Log(reqs.hasEngine);
+		Debug.Log(reqs.numberOfEngines);
+		Debug.Log(reqs.numberOfWings);
+		if (reqs.hasEngine)
+		{
+			aircraftFactory.ManufacturePoweredAircraft(reqs);
+		}
+		else
+		{
+			gliderFactory.ManufactureGlider(reqs);
+		}
+	}
+}
---
+++ AbstractFactory/Assets/Requirements.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Requirements
+{
+    public bool hasEngine;
+    public int numberOfEngines;
+    public int numberOfWings;
+}
